248adf0bd3cc859c8d9f28b17fe6efbff104e3f9
Ian Gabriel Cañas Fernández <52021681+igcf126@users.noreply.github.com>
2023-10-16 16:59:42 -0400

    Add files via upload

---
+++ HolaMundo/HolaMundo.srcs/sources_1/new/HolaMundo.vhd
@@ -0,0 +1,30 @@
+----------------------------------------------------------------------------------
+-- Company: INTEC
+-- Engineer: Ian Gabriel Cañas Fernández - 1092228
+-- 
+-- Create Date: 05/05/2021 11:52:15 PM
+-- Design Name: Hola Mundo
+-- Module Name: Hola Mundo - Behavioral
+-- Project Name: Laboratiorios1092228
+-- Target Devices: Nexys A7
+-- Tool Versions: n/a
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL; 
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity HolaMundo is
+    Port (  entrada1 : in STD_LOGIC;
+            entrada2 : in STD_LOGIC;
+            salida : out STD_LOGIC);
+end HolaMundo;
+
+architecture Behavioral of HolaMundo is
+begin
+
+salida <= entrada1 or entrada2;
+
+end Behavioral;
